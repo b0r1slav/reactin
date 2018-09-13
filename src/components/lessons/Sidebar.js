900310f35dcaa7db4cb0a15bc2003ffc82ad47d5
b0rizz <borizkoff@gmail.com>
2018-09-13 23:01:16 +0300

    hotfix Active Lesson

--- src/components/lessons/Sidebar.js
+++ src/components/lessons/Sidebar.js
@@ -14,13 +14,25 @@ class Sidebar extends Component {
         };
     }
 
+    componentWillReceiveProps(nextProps) {
+        let id = 1;
+
+        if (nextProps.lessonId) {
+            id = parseInt('10', nextProps.lessonId);
+        }
+
+        this.setState({
+            lessonId: id
+        })
+    }
+
     render() {
 
-        const {data} = this.state;
+        const {data, lessonId} = this.state;
 
         const result = data.map((entry, index) => {
             return (
-            <Link key={index} to={"/lessons/" + entry.id}>{entry.title}</Link>
+            <Link key={index} to={'/lessons/' + entry.id} className={lessonId === entry.id ? 'activeLesson': ''} >{entry.title}</Link>
             )
         });
 
