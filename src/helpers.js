abf4dd528065a3cf61609729821aadbaed38e873
b0rizz <borizkoff@gmail.com>
2018-12-13 22:51:58 +0200

    add api url

--- src/helpers.js
+++ src/helpers.js
@@ -1,7 +1,7 @@
 export const url = (path) => {
 
     if (process.env.NODE_ENV === 'production') {
-        return "https://engrexapi.000webhostapp.com/" + path;
+        return "https://engrexapi.herokuapp.com/" + path;
     }
 
     return "http://www.engrexapi.com/" + path;
