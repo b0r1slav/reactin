4b2eeda200b1edcf5591061414d9d6b2d46226e7
b0rizz <borizkoff@gmail.com>
2018-12-18 22:52:17 +0200

    some update

--- src/components/words/WordsItem.js
+++ src/components/words/WordsItem.js
@@ -4,12 +4,14 @@ class WordsItem extends Component {
 
     render() {
 
+        let trans = this.props.data.transcription ? `[${this.props.data.transcription}]` : '';
+
         return (
             <li>
                 {this.props.data.bg}
                 <details>
                     <summary>Answer</summary>
-                    <p>{this.props.data.en}</p>
+                    <p>{this.props.data.en} {trans}</p>
                 </details>
             </li>
         );
