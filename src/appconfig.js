5d5dbfcad8003d6507b06c6b963c4d4c31dcde6f
b0rizz <borizkoff@gmail.com>
2019-01-02 13:11:29 +0200

    update words and phrases versions

--- src/appconfig.js
+++ src/appconfig.js
@@ -2,8 +2,8 @@ export const prodUrl = 'https://engrexapi.herokuapp.com';
 
 export const devUrl = 'http://www.engrexapi.com';
 
-export const wordsVersion = 1.0;
+export const wordsVersion = 1.1;
 
-export const phrasesVersion = 1.0;
+export const phrasesVersion = 1.1;
 
 export const exercisesVersion = 1.0;
