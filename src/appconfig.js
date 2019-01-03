bab858139208559ac295afe0f25aa72484e6ba10
b0rizz <borizkoff@gmail.com>
2019-01-03 10:54:11 +0200

    update version

--- src/appconfig.js
+++ src/appconfig.js
@@ -2,8 +2,8 @@ export const prodUrl = 'https://engrexapi.herokuapp.com';
 
 export const devUrl = 'http://www.engrexapi.com';
 
-export const wordsVersion = 1.1;
+export const wordsVersion = 1.2;
 
-export const phrasesVersion = 1.1;
+export const phrasesVersion = 1.2;
 
 export const exercisesVersion = 1.0;
