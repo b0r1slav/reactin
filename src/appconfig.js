82ebdcdc2efcf3bd77f82eeaccc2ee32b5fef8b8
b0rizz <borizkoff@gmail.com>
2019-01-21 22:03:57 +0200

    update exercises version

--- src/appconfig.js
+++ src/appconfig.js
@@ -6,6 +6,6 @@ export const wordsVersion = 1.3;
 
 export const phrasesVersion = 1.2;
 
-export const exercisesVersion = 1.1;
+export const exercisesVersion = 1.2;
 
 export const wordsCompetitionVersion = 1.0;
